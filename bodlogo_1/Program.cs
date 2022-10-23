898a234f7903c403f41b76d0aed9c5a05a6acc26
dorjoo <dorjoohover@gmail.com>
2022-10-23 21:15:18 +0800

    Add project files.

---
+++ bodlogo_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bodlogo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+
+            List<int> num = PrimaryNumber(int.Parse(n));
+            for (int i = 0; i < num.Count; i++)
+            {
+                Console.WriteLine(num[i] );
+            }
+        }
+
+        static List<int> PrimaryNumber(int num)
+        {
+            List<int> nums = new List<int>();
+
+            for(int j = 2; j <= num; j++)
+            {
+                if(j == 2)
+                {
+                    nums.Add(j);
+                }
+                else
+                {
+                    int i = 2, check = 0 ;
+                    while (i < j)
+                    {
+                        if( j % i == 0)
+                        {
+                            check++;
+                            break;
+                        }
+                        i++;
+                    }
+                    if (check == 0 && )
+                    {
+                        nums.Add(i);
+                    }
+                    
+                }
+            }
+            return nums;
+        }
+    }
+}
